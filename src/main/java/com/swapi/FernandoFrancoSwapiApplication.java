bd7058c4eff6dedd83367eb9d89cd7c8d39d58a9
Fernando Pessoa Franco <fernandopessoafranco@gmail.com>
2019-11-01 20:26:28 -0300

    First commit

---
+++ src/main/java/com/swapi/FernandoFrancoSwapiApplication.java
@@ -0,0 +1,13 @@
+package com.swapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FernandoFrancoSwapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FernandoFrancoSwapiApplication.class, args);
+	}
+
+}
